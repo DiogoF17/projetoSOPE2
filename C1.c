f2202d2ad7da07c20b492308e1c14ab5f9e981da
diogo santos <diogofilipe1_santos@hotmail.com>
2020-04-17 13:55:08 +0100

    Comunicacao do utente para a casa de banho feita

--- C1.c
+++ C1.c
@@ -5,6 +5,12 @@
 #include <sys/stat.h>
 #include <string.h>
 
+struct ParametrosParaFifo{
+    int identificador; //identificador de cada pedido
+    int tempo; //milisegundos
+    char fifo_resp[100]; //nome do fifo que vai recolher a resposta
+};
+
 int isZero(char *string){
     for(int i = 0; i < strlen(string); i++){
         if(string[i] != 48)
@@ -74,10 +80,80 @@ void validFormat(int argc, char *argv[]){
     */
 }
 
+void find_fifo_name(char *argv[], char *string){
+    int i = 3;
+
+    while(argv[i] != NULL){
+        if(strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "-n") == 0 || validNumber(argv[i]) == 1)
+            continue;
+        else{
+            strcpy(string, argv[i]);
+            break;
+        }
+    }
+}
+/*
+void *thread_func(void *arg){
+    int escritor;
+
+    int time = (rand() % 5) + 1;
+
+    char fifo_resp[100];
+    sprintf(fifo_resp, "/tmp/fifo_resp_%ld", pthread_self());
+
+    struct ParametrosParaFifo argFifo;
+
+    argFifo.identificador = (*(struct ParametrosParaThread *)arg).identificador;
+    argFifo.tempo = time;
+    strcpy(argFifo.fifo_resp, fifo_resp);
+    
+    do{
+        escritor = open((*(struct ParametrosParaThread *)arg).fifo_ped, O_WRONLY);
+    }while(escritor == -1);
+
+    //printf("Lanca %d -- %d -- %s\n", argFifo.identificador, argFifo.tempo, argFifo.fifo_resp);
+    write(escritor, &argFifo, sizeof(struct ParametrosParaFifo));
+    
+    free(arg);
+    close(escritor);
+
+    return NULL;
+}*/
+
 int main(int argc, char *argv[]){
+
+    pthread_t tid;
 
     validFormat(argc, argv);
 
-    return 0;
+    char dirFifoPed[100], fifo_ped[100];
+    find_fifo_name(argv, fifo_ped);
+    sprintf(dirFifoPed, "/tmp/%s", fifo_ped);
+
+    mkfifo(dirFifoPed, 0660);
+    int leitor = open(dirFifoPed, O_RDONLY);
+
+    struct ParametrosParaFifo argFifo;
+
+    int numLidos;
+
+    do{
+
+        numLidos = read(leitor, &argFifo, sizeof(struct ParametrosParaFifo));
+
+        /*void *arg = malloc(sizeof(struct ParametrosParaFifo));
+        *(struct ParametrosParaFifo *)arg = argFifo;
+    
+        pthread_create(&tid, NULL, thread_func, arg);*/
+
+        if(argFifo.identificador != 0)
+            printf("Lanca %d -- %d -- %s\n", argFifo.identificador, argFifo.tempo, argFifo.fifo_resp);
+
+    } while(numLidos > 0);
+
+    close(leitor);
+    unlink(dirFifoPed);
+
+    return 0;;
 
 }
--- U1.c
+++ U1.c
@@ -5,6 +5,18 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
+
+struct ParametrosParaFifo{
+    int identificador; //identificador de cada pedido
+    int tempo; //milisegundos
+    char fifo_resp[100]; //nome do fifo que vai recolher a resposta
+};
+
+struct ParametrosParaThread{
+    int identificador; //identificador de cada thread na linha de lanacamento
+    char fifo_ped[100]; //nome do fifo para o qual metemos os parametros
+};
 
 int isZero(char *string){
     for(int i = 0; i < strlen(string); i++){
@@ -42,28 +54,77 @@ void validFormat(int argc, char *argv[]){
 }
 
 void *thread_func(void *arg){
-    printf("Lanca %d\n", *(int *)arg);
+    int escritor;
+
+    int time = (rand() % 5) + 1;
+
+    char fifo_resp[100];
+    sprintf(fifo_resp, "/tmp/fifo_resp_%ld", pthread_self());
+
+    struct ParametrosParaFifo argFifo;
+
+    argFifo.identificador = (*(struct ParametrosParaThread *)arg).identificador;
+    argFifo.tempo = time;
+    strcpy(argFifo.fifo_resp, fifo_resp);
+    
+    do{
+        escritor = open((*(struct ParametrosParaThread *)arg).fifo_ped, O_WRONLY);
+    }while(escritor == -1);
+
+    //printf("Lanca %d -- %d -- %s\n", argFifo.identificador, argFifo.tempo, argFifo.fifo_resp);
+    write(escritor, &argFifo, sizeof(struct ParametrosParaFifo));
+    
     free(arg);
+    close(escritor);
+
     return NULL;
+}
+
+void find_fifo_name(char *argv[], char *string){
+    int i = 3;
+
+    while(argv[i] != NULL){
+        if(strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "-n") == 0 || validNumber(argv[i]) == 1)
+            continue;
+        else{
+            strcpy(string, argv[i]);
+            break;
+        }
+    }
 }
 
 int main(int argc, char *argv[]){
 
     validFormat(argc, argv);
 
-    int identificador = 0;
+    srand(time(NULL));
+
+    int identificador = 1;
     double num = 0, numSecs = atoi(argv[2]) * 1000;
     pthread_t tid;
 
+    char dirFifoPed[100], fifo_ped[100];
+    find_fifo_name(argv, fifo_ped);
+    sprintf(dirFifoPed, "/tmp/%s", fifo_ped);
+
     while(num < numSecs){
         usleep(50000); //sleep de 50 milisegundo
-        //printf("Lanca %d\n", identificador);
-        void * arg = malloc (sizeof(int));
-        *(int *) arg = identificador;
+        
+        void * arg = malloc (sizeof(struct ParametrosParaThread));
+        (*(struct ParametrosParaThread *) arg).identificador = identificador;
+        strcpy((*(struct ParametrosParaThread *) arg).fifo_ped, dirFifoPed);
+
         pthread_create(&tid, NULL, thread_func, arg);
         num+=50;
         identificador++;
     }
+
+    void * arg = malloc (sizeof(struct ParametrosParaThread));
+    (*(struct ParametrosParaThread *) arg).identificador = 0;
+    strcpy((*(struct ParametrosParaThread *) arg).fifo_ped, dirFifoPed);
+
+    pthread_create(&tid, NULL, thread_func, arg);
+
 
     pthread_exit(0);
 
